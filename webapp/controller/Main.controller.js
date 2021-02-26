d757248816f4c99c9853faab1b1839551358f1e0
Fabio Pagoti <fabiopagoti@gmail.com>
2021-02-26 21:52:43 +0000

    Aula 12

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -8,8 +8,38 @@ sap.ui.define([
 		"use strict";
 
 		return Controller.extend("ovly.odatawrite.controller.Main", {
-			onInit: function () {
 
-			}
+            oList: null,
+
+			onInit: function () {
+                this.oList = this.byId("list");
+            },
+
+
+            onItemPress: function(oEvent){
+                var oSource = oEvent.getSource();
+                var oParameters = oEvent.getParameters();
+                var oListItem = oParameters.listItem;
+            },
+
+            onLiveChange: function(oEvent){
+                
+                var aFilter = [];
+                var sNewValue = oEvent.getParameters().newValue;
+                if(sNewValue){
+                    aFilter.push(
+                        new sap.ui.model.Filter({
+                            path: "Name",
+                            operator: sap.ui.model.FilterOperator.Contains,
+                            value1: sNewValue
+                        })
+                    );
+                }
+
+                var oBinding = this.oList.getBinding("items"); // sap.ui.model.odata.v2.ODataListBinding
+
+                oBinding.filter(aFilter);
+            }
+
 		});
 	});
